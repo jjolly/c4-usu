f33a585972514aa63a775dd2cc6f56dac1596215
John Jolly <jjolly@cs.utah.edu>
2022-03-19 06:01:04 -0600

    AI REST API at games/api/v1/move/[0-2]{42}

--- connect4/game/urls.py
+++ connect4/game/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    re_path(r'^api/v1/move/(?P<pk>[0-2]{42})$', views.move_from_board)
 ]
